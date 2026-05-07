9b42c39638c8612ac23986681191e09e0b5f6d31
Ravi pratap singh bhadoriya <ravipratapsinghbhadoriya@Ravis-MacBook-Pro.local>
2026-05-07 06:23:03 +0530

    adding test folder/file

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+
+def test_ready():
+    client = app.test_client()
+    response = client.get("/ready")
+    assert response.status_code == 200
