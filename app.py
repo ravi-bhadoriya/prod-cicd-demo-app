e4a10519791c71edce6b4dd2e0b70719937f27dd
Ravi pratap singh bhadoriya <ravipratapsinghbhadoriya@Ravis-MacBook-Pro.local>
2026-05-09 20:02:22 +0530

    NEW APP

--- app.py
+++ app.py
@@ -10,14 +10,29 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
 HOSTNAME = socket.gethostname()
 START_TIME = time.time()
 
+
 @app.route("/")
 def home():
-    return {
-        "application": "prod-cicd-demo",
-        "version": VERSION,
-        "environment": ENVIRONMENT,
-        "hostname": HOSTNAME
-    }
+    return f"""
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MY PROD APP</title>
+      </head>
+      <body>
+        <h1>MY PROD APP</h1>
+        <pre>{{
+  "application": "prod-cicd-demo",
+  "version": "{VERSION}",
+  "environment": "{ENVIRONMENT}",
+  "hostname": "{HOSTNAME}"
+}}</pre>
+      </body>
+    </html>
+    """
+
 
 @app.route("/health")
 def health():
@@ -26,11 +41,13 @@ def health():
         "uptime": round(time.time() - START_TIME, 2)
     }
 
+
 @app.route("/ready")
 def ready():
     return {
         "status": "ready"
     }
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
