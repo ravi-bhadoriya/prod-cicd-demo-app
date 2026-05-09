d94ba58f6ec4beaf3bd55f15bbc6a62514573d06
Ravi pratap singh bhadoriya <ravipratapsinghbhadoriya@Ravis-MacBook-Pro.local>
2026-05-09 22:42:45 +0530

    bad canary

--- app.py
+++ app.py
@@ -11,28 +11,32 @@ HOSTNAME = socket.gethostname()
 START_TIME = time.time()
 
 
+# @app.route("/")
+# def home():
+#     return f"""
+#     <!DOCTYPE html>
+#     <html lang="en">
+#       <head>
+#         <meta charset="UTF-8" />
+#         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+#         <title>MY PROD APP</title>
+#       </head>
+#       <body>
+#         <h1>MY PROD APP</h1>
+#         <pre>{{
+#   "application": "prod-cicd-demo",
+#   "version": "{VERSION}",
+#   "environment": "{ENVIRONMENT}",
+#   "hostname": "{HOSTNAME}",
+#   "deployment_strategy": "recreate"
+# }}</pre>
+#       </body>
+#     </html>
+#     """
+
 @app.route("/")
 def home():
-    return f"""
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>MY PROD APP</title>
-      </head>
-      <body>
-        <h1>MY PROD APP</h1>
-        <pre>{{
-  "application": "prod-cicd-demo",
-  "version": "{VERSION}",
-  "environment": "{ENVIRONMENT}",
-  "hostname": "{HOSTNAME}",
-  "deployment_strategy": "recreate"
-}}</pre>
-      </body>
-    </html>
-    """
+    raise Exception("bad canary release")
 
 
 @app.route("/health")
